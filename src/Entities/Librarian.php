253200b315107f3b0165697eaf51b244be6d03f2
badrbelabrik <badr.belabrik@gmail.com>
2026-05-05 16:46:51 +0100

    librarian addbook method

--- src/Entities/Librarian.php
+++ src/Entities/Librarian.php
@@ -8,10 +8,29 @@ class Librarian
     public function __construct(){
         $this->id = self::$nextId++;
     }
-    public function registerMember(Member $member):void{
+
+    public function registerMember(Library $library,Member $member):void{
 
     }
-    public function addBook(Book $book):void{
+    public function addBook(Library $library):void{
+        echo "\n--- ADD NEW BOOK ---\n";
+        $isbn = trim(readline("Enter the ISBN: "));
+        $title = trim(readline("Enter the title of the book: "));
+        $author = trim(readline("Enter the author name: "));
 
+        $newBook = new Book($isbn,$title,$author);
+        $library->addBook($newBook);
+
+        echo "The book ".$newBook->getTitle()."added successfully !";
+    }
+
+    public function addMember(Library $library):void{
+        echo "\n--- ADD NEW MEMBER ---\n";
+        $name = trim(readline("Enter the member name :"));
+        $email = trim(readline("Enter the email :"));
+        $type = trim(readline("Enter the member type (student or professor): "));
+
+        $newMember = new Member($name,$email,$type);
+        $library->registerMember($newMember);
     }
 }
--- src/Services/Library.php
+++ src/Services/Library.php
@@ -7,15 +7,17 @@ class Library
     private string $name;
     private array $books;
     private array $users;
+    private array $librarians;
+    private array $members;
 
     public function __construct(string $name){
         $this->name = $name;
     }
     public function addBook(Book $book):void{
-
+        array_push($this->books,$book);
     }
     public function registerMember(Member $member):void{
-
+        array_push($this->members);
     }
     public function borrowBook(Member $member,Book $book):void{
 
