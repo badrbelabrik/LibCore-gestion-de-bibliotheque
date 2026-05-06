c36ee25419283191dce4073e9f96a6fe1e8e0766
Ihsane-12 <benminaihssane@gmail.com>
2026-05-06 10:04:30 +0100

    Complete Book entity with attributes, getters, setters, and availability management

--- src/Entities/Book.php
+++ src/Entities/Book.php
@@ -5,17 +5,39 @@ namespace Entities;
 class Book
 {
     private string $isbn;
+    private  string $title;
     private string $author;
     private string $state;
     private bool $isAvailable;
 
     public function __construct(string $isbn,string $author){
     $this->isbn = $isbn;
+    $this->title = $title;
     $this->author = $author;
     $this->state = "disponible";
     $this->isAvailable = true;
     }
 
-    public function getIsbn(): string
+    public function getIsbn(): string{
+        return $this->isbn;
     }
+    public function getTitle(): string{
+        return $this->title;
+    }
+    public function getAuthor(): string{
+        return $this->author;
+    }
+    public function getState(): string{
+        return $this->state;
+    }
+    public function isAvailable(): bool{
+        return $this->isAvailable;
+    }
+    public function setIsAvailable(bool $status): void{
+        $this->isAvailable = $status;
+    }
+    public function setState(string $state): void{
+        $this->state = $state;
+    }
+
 }
