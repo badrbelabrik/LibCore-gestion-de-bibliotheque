5e066d9a571be16de142e92f5a4d08f84c64d2b6
Ihsane-12 <benminaihssane@gmail.com>
2026-05-06 09:52:27 +0100

    Implement Library class methods and initialize collections; add basic Book structure

--- src/Entities/Book.php
+++ src/Entities/Book.php
@@ -4,18 +4,18 @@ namespace Entities;
 
 class Book
 {
-    private static int $nextId = 1;
     private string $isbn;
-    private int $id;
     private string $author;
     private string $state;
     private bool $isAvailable;
 
     public function __construct(string $isbn,string $author){
     $this->isbn = $isbn;
-    $this->id = self::$nextId++;
     $this->author = $author;
     $this->state = "disponible";
     $this->isAvailable = true;
+    }
+
+    public function getIsbn(): string
     }
 }
--- src/Services/Library.php
+++ src/Services/Library.php
@@ -10,17 +10,42 @@ class Library
 
     public function __construct(string $name){
         $this->name = $name;
+        $this->books = [];
+        $this->users = [];
     }
     public function addBook(Book $book):void{
+        $this->books[] = $book;
 
     }
     public function registerMember(Member $member):void{
+        $this->users[] = $member;
 
     }
     public function borrowBook(Member $member,Book $book):void{
+        if($book->isAvailable()){
+            $member->borrowBook($book);
+        }else{
+            echo "Book is not available";
+        }
 
     }
     public function returnBook(Member $Member,Book $Book):void{
+        $Member->returnBook($Book);
 
     }
+    public function searchBook(string $Keyword):array
+    {
+        $result = [];
+        foreach ($this->books as $book) {
+            if (
+                stripos($book->getitle(), $Keyword) !== false ||
+                stripos($book->geAuthor(), $Keyword) !== false
+            ) {
+                $result[] = $book;
+            }
+        }
+        return $result;
+
+    }
+
 }
