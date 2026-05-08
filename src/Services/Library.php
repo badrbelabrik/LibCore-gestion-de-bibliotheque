95db3392da486072d5baad33cdd0acd1ceee78da
badrbelabrik <badr.belabrik@gmail.com>
2026-05-08 17:40:08 +0100

    use env file to get database configuration

--- src/Services/Library.php
+++ src/Services/Library.php
@@ -10,19 +10,19 @@ use PDOException;
 class Library
 {
     private string $name;
-    private PDO $con;
+    private PDO $db;
 
     public function __construct(string $name,PDO $con){
-        $this->con = $con;
+        $this->db = $con;
         $this->name = $name;
     }
     public function addBook(Book $book):void{
         try{
             $sql = "INSERT INTO books (isbn, title, author) VALUES (?, ?, ?)";
-            $stmt = $this->con->prepare($sql);
+            $stmt = $this->db->prepare($sql);
             $stmt->execute([$book->getIsbn(), $book->getTitle(), $book->getAuthor()]);
 
-            echo "Book ".$book->getTitle()."added successfully ! \n";
+            echo "Book ".$book->getTitle()." added successfully ! \n";
         } catch(PDOException $e){
             echo "Error: ".$e->getMessage();
         }
@@ -31,7 +31,7 @@ class Library
     public function deleteBook($id):void{
         try{
             $sql = "SELECT * FROM books WHERE id = ?";
-            $stmt = $this->con->prepare($sql);
+            $stmt = $this->db->prepare($sql);
             $stmt->execute([$id]);
             $book = $stmt->fetch(PDO::FETCH_ASSOC);
 
@@ -39,13 +39,13 @@ class Library
                 echo "Book not found !";
                 return;
             }
-            if($book['state'] != "Deleted"){
+            if($book['state'] != "Deleted" && $book['isAvailable'] == true){
                 $delSql = "UPDATE books SET isAvailable = false,state = 'Deleted' WHERE id = ?";
-                $stmt = $this->con->prepare($delSql);
+                $stmt = $this->db->prepare($delSql);
                 $stmt->execute([$id]);
                 echo"Book deleted successfully ! \n";
             } else{
-                echo "The book already deleted from the catalogue !";
+                echo "You cannot delete this book";
             }
 
         } catch(PDOException $e){
@@ -56,14 +56,14 @@ class Library
     public function markBookUnderRepair($id){
         try{
             $sql = "SELECT * FROM books WHERE id = ?";
-            $stmt = $this->con->prepare($sql);
+            $stmt = $this->db->prepare($sql);
             $stmt->execute([$id]);
             $book = $stmt->fetch(PDO::FETCH_ASSOC);
             if($book['state'] == 'En réparation'){
                 echo "The book is already under repair\n";
             } else if($book['isAvailable'] == true && $book['state'] == 'Disponible'){
                 $repairSql = "UPDATE books SET isAvailable = false, state='En réparation' WHERE id = ?";
-                $stmt = $this->con->prepare($repairSql);
+                $stmt = $this->db->prepare($repairSql);
                 $stmt->execute([$id]);
                 echo "The book marked under repair !\n";
             } else{
@@ -77,7 +77,7 @@ class Library
         try{
             $sql = "INSERT INTO members (name,email,member_type) VALUES (?, ?, ?)";
 
-            $stmt = $this->con->prepare($sql);
+            $stmt = $this->db->prepare($sql);
             $stmt->execute([$member->getName(),$member->getEmail(),$member->getType()]);
 
             echo "Member ".$member->getName()." added successfully ! \n";
@@ -89,7 +89,7 @@ class Library
     public function showAllBooks():void{
         try{
             $sql = "SELECT * FROM books WHERE state != 'Deleted'";
-            $stmt = $this->con->prepare($sql);
+            $stmt = $this->db->prepare($sql);
             $stmt->execute();
             $books = $stmt->fetchAll(PDO::FETCH_ASSOC);
         } catch(PDOException $e){
